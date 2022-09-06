d46402b2bc2e26e1f0af0a0c27c31bf616b909a2
Aidan Killeen <aidan.killeen@gmail.com>
2022-09-06 11:34:50 +0100

    Added polymorphism test

---
+++ HrSystem/PolymorphismTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HrSystem
+{
+    class PolymorphismTest
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Polymorphism Test");
+
+            Person[] team = new Person[]
+            {
+                new Person("Alice", "Adams"), 
+                new Employee("Bob", "Burns", "E12345"), 
+                new Contractor("Carol", "Conway", "CPL"), 
+                new Manager("Danielle", "Daly", "E1111", 15)
+            };
+
+            for (int i=0; i<team.Length; i++)
+            {
+                team[i].Display();
+            }
+
+        }
+    }
+}
