3839d1685ba0886d83eecf495e7eff34597d5ade
Aidan Killeen <aidan.killeen@gmail.com>
2022-09-06 15:53:56 +0100

    Added StaticInvestigation

---
+++ StaticInvestigation/Message.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticInvestigation
+{
+    class Message
+    {
+        public static int count = 0;
+
+        public string Title { get; set; }
+        public string Text { get; set; }
+
+        public Message(string title, string text)
+        {
+            Title = title;
+            Text = text;
+            count++;
+        }
+
+        public static void DisplayCount()
+        {
+            Console.WriteLine($"The count is {count}");
+        }
+    }
+}
---
+++ StaticInvestigation/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace StaticInvestigation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Static Investigation");
+
+            Message m1 = new Message("Message 1", "this is message 1");
+            Console.WriteLine(Message.count);
+
+            Message m2 = new Message("Message 2", "this is message 2");
+            Console.WriteLine(Message.count);
+
+            Message m3 = new Message("Message 3", "this is message 3");
+            Console.WriteLine(Message.count);
+
+
+            Message.DisplayCount();
+
+        }
+    }
+}
