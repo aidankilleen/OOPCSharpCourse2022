6b3c0cad83a94e3f72a8c9681965d685e4274b3f
Aidan Killeen <aidan.killeen@gmail.com>
2022-09-09 15:06:40 +0100

    Added SQL Server db

---
+++ SqlServerHelloWorld/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace SqlServerHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("SqlServer Hello World");
+            SqlConnection conn;
+
+            string connectionString = "Server=tcp:nottheserver.database.windows.net,1433;Initial Catalog=trainingdb;Persist Security Info=False;User ID=nottheuser;Password=notthepassword;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            conn = new SqlConnection(connectionString);
+
+            conn.Open();
+            string sql = "SELECT * FROM users";
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            SqlDataReader rdr = cmd.ExecuteReader();
+
+            while (rdr.Read())
+            {
+                string name = rdr.GetString(1);
+                Console.WriteLine(name);
+            }
+            rdr.Close();
+            conn.Close();
+        }
+    }
+}
